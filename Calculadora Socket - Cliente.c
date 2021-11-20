3cb4c14d0a3e6fd098c9a1a0718a738135880050
Matheus Moreira Mendes <60854702+MatheusMoreira6@users.noreply.github.com>
2021-11-20 14:55:59 -0400

    Add Client

---
+++ Calculadora Socket - Cliente.c
@@ -0,0 +1,195 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <winsock2.h>
+#include <conio.h>
+#include <locale.h>
+
+#define PORTA 2021
+#define T_MAX 1024
+
+int main(){
+    setlocale(LC_ALL, "Portuguese");
+    system("title Cliente - Calculadora Socket");
+    system("color F0");
+
+    //Iniciliazação do Winsock.
+    WSADATA winsock;
+    if(WSAStartup(MAKEWORD(2, 2), &winsock) != 0) {
+        printf("\nFalha na inicializaçãoo da biblioteca winsock!\n");
+        return(1);
+    }
+
+    //Criação do Socket.
+    SOCKET socket_aplicacao = socket(AF_INET, SOCK_STREAM, 0);
+    if(socket_aplicacao == INVALID_SOCKET) {
+        printf("\nFalha na criação do socket. Erro: %d\n", WSAGetLastError());
+        WSACleanup();
+        return(1);
+    }
+
+    //Configuração do servidor.
+    struct sockaddr_in servidor;
+    memset(&servidor, 0, sizeof(servidor));
+    servidor.sin_addr.s_addr = inet_addr("127.0.0.1");
+    servidor.sin_family = AF_INET;
+    servidor.sin_port = htons(PORTA);
+
+    //Conexão com o servidor.
+    if(connect(socket_aplicacao, (struct sockaddr *)&servidor, sizeof(servidor)) < 0){
+        printf("\nFalha na conexão com o servidor. Erro: %d\n", WSAGetLastError());
+        closesocket(socket_aplicacao);
+        WSACleanup();
+        return(1);
+    }
+
+    //Receber uma mensagem.
+    char mensagem[T_MAX];
+    if(recv(socket_aplicacao, mensagem, T_MAX, 0) <= 0){
+        printf("\nFalha ao receber a messagem do servidor. Erro: %d\n", WSAGetLastError());
+        closesocket(socket_aplicacao);
+        WSACleanup();
+        return(1);
+    }
+    mensagem[strlen(mensagem)] = '\0';
+    printf("Mensagem recebida: %s", mensagem);
+
+    printf("\nPressione qualquer tecla para continuar!");
+    getche();
+
+    //Loop para o envio e recebimento de mensagens.
+    char verificador;
+    do {
+        //Limpando a estrutura.
+        memset(mensagem, 0x0, T_MAX);
+        system("cls");
+
+        //Variavel de controle.
+        verificador='n';
+
+        //Aguardando por menu.
+        if(recv(socket_aplicacao, mensagem, T_MAX, 0) <= 0){
+            printf("\nFalha ao receber a messagem do servidor. Erro: %d\n", WSAGetLastError());
+            closesocket(socket_aplicacao);
+            WSACleanup();
+            return(1);
+        }
+        mensagem[strlen(mensagem)] = '\0';
+
+        //Exibir menu.
+        printf("%s", mensagem);
+        memset(mensagem, 0x0, T_MAX);
+
+        //Pegar opção do menu.
+        fflush(stdin);
+        gets(mensagem);
+
+        //Enviar opção.
+        if(send(socket_aplicacao, mensagem, strlen(mensagem), 0) < 0){
+            printf("\nFalha ao enviar a mensagem. Erro: %d\n", WSAGetLastError());
+            closesocket(socket_aplicacao);
+            WSACleanup();
+            return(1);
+        }
+        memset(mensagem, 0x0, T_MAX);
+
+        //Aguardando mensagem solicitando o primeiro número.
+        if(recv(socket_aplicacao, mensagem, T_MAX, 0) <= 0){
+            printf("\nFalha ao receber a messagem do servidor. Erro: %d\n", WSAGetLastError());
+            closesocket(socket_aplicacao);
+            WSACleanup();
+            return(1);
+        }
+        mensagem[strlen(mensagem)] = '\0';
+
+        //Exibir mensagem.
+        printf("%s", mensagem);
+        memset(mensagem, 0x0, T_MAX);
+
+        //Pegar primeiro número.
+        fflush(stdin);
+        gets(mensagem);
+
+        //Enviar primeiro número.
+        if(send(socket_aplicacao, mensagem, strlen(mensagem), 0) < 0){
+            printf("\nFalha ao enviar a mensagem. Erro: %d\n", WSAGetLastError());
+            closesocket(socket_aplicacao);
+            WSACleanup();
+            return(1);
+        }
+        memset(mensagem, 0x0, T_MAX);
+
+        //Aguardando mensagem solicitando o segundo número.
+        if(recv(socket_aplicacao, mensagem, T_MAX, 0) <= 0){
+            printf("\nFalha ao receber a messagem do servidor. Erro: %d\n", WSAGetLastError());
+            closesocket(socket_aplicacao);
+            WSACleanup();
+            return(1);
+        }
+        mensagem[strlen(mensagem)] = '\0';
+
+        //Exibir mensagem.
+        printf("%s", mensagem);
+        memset(mensagem, 0x0, T_MAX);
+
+        //Pegar segundo número.
+        fflush(stdin);
+        gets(mensagem);
+
+        //Enviar segundo número.
+        if(send(socket_aplicacao, mensagem, strlen(mensagem), 0) < 0){
+            printf("\nFalha ao enviar a mensagem. Erro: %d\n", WSAGetLastError());
+            closesocket(socket_aplicacao);
+            WSACleanup();
+            return(1);
+        }
+        memset(mensagem, 0x0, T_MAX);
+
+        //Aguardando mensagem com o resultado.
+        if(recv(socket_aplicacao, mensagem, T_MAX, 0) <= 0){
+            printf("\nFalha ao receber a messagem do servidor. Erro: %d\n", WSAGetLastError());
+            closesocket(socket_aplicacao);
+            WSACleanup();
+            return(1);
+        }
+        mensagem[strlen(mensagem)] = '\0';
+
+        //Exibir mensagem.
+        printf("%s", mensagem);
+        memset(mensagem, 0x0, T_MAX);
+
+        //Aguardando mensagem se deseja realizar uma nova operação.
+        if(recv(socket_aplicacao, mensagem, T_MAX, 0) <= 0){
+            printf("\nFalha ao receber a messagem do servidor. Erro: %d\n", WSAGetLastError());
+            closesocket(socket_aplicacao);
+            WSACleanup();
+            return(1);
+        }
+        mensagem[strlen(mensagem)] = '\0';
+
+        //Exibir mensagem.
+        printf("%s", mensagem);
+        memset(mensagem, 0x0, T_MAX);
+
+        //Pegar resposta da decisão.
+        fflush(stdin);
+        verificador=getche();
+
+        //Enviar resposta da decisão.
+        mensagem[0]=verificador;
+        mensagem[strlen(mensagem)]='\0';
+        if(send(socket_aplicacao, mensagem, strlen(mensagem), 0) < 0){
+            printf("\nFalha ao enviar a mensagem. Erro: %d\n", WSAGetLastError());
+            closesocket(socket_aplicacao);
+            WSACleanup();
+            return(1);
+        }
+        memset(mensagem, 0x0, T_MAX);
+
+        //Verificação para repetir.
+    }while(verificador=='s' || verificador=='S');
+
+    //Fechando o socket.
+    closesocket(socket_aplicacao);
+    WSACleanup();
+    return(0);
+}
